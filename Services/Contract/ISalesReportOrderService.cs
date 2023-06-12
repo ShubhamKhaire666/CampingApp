7f13c94595e28777351055c4dd0dc804c0c99acc
ShubhamKhaire666 <shubhamkhaire.edu@gmail.com>
2023-06-12 19:19:09 +0300

    Dashboard

--- Services/Contract/ISalesReportOrderService.cs
+++ Services/Contract/ISalesReportOrderService.cs
@@ -4,7 +4,12 @@ namespace CampingApp.Services.Contract
 {
 	public interface ISalesReportOrderService
 	{
+		//SR
 		Task<List<GroupedFieldPriceModel>> GetEmployeePricePerMonthData();
 		Task<List<GroupedFieldQtyModel>> GetQtyPerProductCategory();
+		Task<List<GroupedFieldQtyModel>> GetQtyPerMonthData();
+
+		//TL
+		Task<List<GroupedFieldPriceModel>> GetGrossSalesPerTeamMemberData();
 	}
 }
--- Services/SalesOrderReportService.cs
+++ Services/SalesOrderReportService.cs
@@ -50,6 +50,47 @@ namespace CampingApp.Services
 			}
 		}
 
+		public Task<List<GroupedFieldPriceModel>> GetGrossSalesPerTeamMemberData()
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<List<GroupedFieldQtyModel>> GetQtyPerMonthData()
+		{
+			try
+			{
+				var reportData = await(from s in dbContext.Sales
+									   where s.EmployeeId == 9
+									   group s by s.OrderDateTime.Month into GroupedData
+									   orderby GroupedData.Key
+									   select new GroupedFieldQtyModel
+									   {
+										   GroupedFieldKey =
+										   (
+											   GroupedData.Key == 1 ? "Jan" :
+											   GroupedData.Key == 2 ? "Feb" :
+											   GroupedData.Key == 3 ? "Mar" :
+											   GroupedData.Key == 4 ? "Apr" :
+											   GroupedData.Key == 5 ? "May" :
+											   GroupedData.Key == 6 ? "Jun" :
+											   GroupedData.Key == 7 ? "Jul" :
+											   GroupedData.Key == 8 ? "Aug" :
+											   GroupedData.Key == 9 ? "Sep" :
+											   GroupedData.Key == 10 ? "Oct" :
+											   GroupedData.Key == 11 ? "Nov" :
+											   GroupedData.Key == 12 ? "Dec" :
+											   ""
+										   ),
+										   Qty = GroupedData.Sum(o => o.OrderItemQty)
+									   }).ToListAsync();
+				return reportData;
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+		}
+
 		public async Task<List<GroupedFieldQtyModel>> GetQtyPerProductCategory()
 		{
 
